c40819348b2ac92b34744c66abd1c46c74fe0dde
seregero00 <seregero18@gmail.com>
2022-11-07 12:18:54 +0100

    Probando Colisiones

--- citm_desvj_project_template-L08/Game/Source/Map.cpp
+++ citm_desvj_project_template-L08/Game/Source/Map.cpp
@@ -192,8 +192,9 @@ bool Map::Load()
     }
     
     // L07 DONE 3: Create colliders
+    // //
     // Later you can create a function here to load and create the colliders from the map
-    PhysBody* c1 = app->physics->CreateRectangle(224 + 128, 543 + 32, 256, 64, STATIC);
+    PhysBody* c1 = app->physics->CreateRectangle(0 + 256/2, 288 + 64/2, 256, 64, STATIC);
     // L07 DONE 7: Assign collider type
     c1->ctype = ColliderType::PLATFORM;
 
