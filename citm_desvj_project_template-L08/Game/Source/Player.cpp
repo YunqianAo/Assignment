f74e31415c6724fe27723bdf808980872f2d6796
seregero00 <seregero18@gmail.com>
2022-11-14 10:43:51 +0100

    texture player

--- citm_desvj_project_template-L08/Game/Source/Player.cpp
+++ citm_desvj_project_template-L08/Game/Source/Player.cpp
@@ -39,11 +39,12 @@ bool Player::Awake() {
 
 	//L02: DONE 1: Initialize Player parameters
 	//pos = position;
-	texturePath = "Assets/Textures/player.png";
+	
 
 	//L02: DONE 5: Get Player parameters from XML
 	position.x = parameters.attribute("x").as_int();
 	position.y = parameters.attribute("y").as_int();
+	texturePath = parameters.attribute("texturepath").as_string();
 	//texturePath = parameters.attribute("texturepath").as_string();
 
 	return true;
